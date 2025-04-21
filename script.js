14e7276d560e49e1d15fc704975364ea000b043f
Mikołaj Kokot <panmeek@proton.me>
2025-04-21 18:55:44 +0200

    Add paint function, make grid size adjustable

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const gridContainer = document.querySelector(".grid-container");
+const gridSizeSelector = document.querySelector(".grid-size-selector");
+gridSizeSelector.addEventListener("input", (event) => {
+    gridSize = event.target.value;
+    createGrid(gridSize);
+})
 
-function createGrid() {
-    for (let i = 0; i < 16; i++) {
+function createGrid(gridSize = 16) {
+    gridContainer.innerHTML = "";
+
+    for (let i = 0; i < gridSize; i++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("grid-row");
 
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < gridSize; j++) {
             const gridElement = document.createElement("div");
             gridElement.classList.add("grid-element");
+            gridElement.addEventListener("mouseover", paintElement);
 
             gridRow.appendChild(gridElement);
         }
 
         gridContainer.appendChild(gridRow);
     }
+}
+
+function paintElement(event) {
+    event.target.style.backgroundColor = "black";
 }
 
 createGrid();
